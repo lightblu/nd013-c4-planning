f69f4e1c2be643644e655937a6b801d4a80a4e17
Bjoern Lichtblau <b.lichtblau@fu-berlin.de>
2023-05-15 00:41:57 +0200

    Fill all TODOs
    Fill all TODOs (first attempt)

--- project/starter_files/motion_planner.cpp
+++ project/starter_files/motion_planner.cpp
@@ -82,9 +82,9 @@ std::vector<State> MotionPlanner::generate_offset_goals(
   // main goal. To get a perpendicular angle, just add 90 (or PI/2) to the main
   // goal heading.
 
-  // TODO-Perpendicular direction: ADD pi/2 to the goal yaw
+  // TODOX-Perpendicular direction: ADD pi/2 to the goal yaw
   // (goal_state.rotation.yaw)
-  //auto yaw = ;  // <- Fix This
+  auto yaw = goal_state.rotation.yaw + (M_PI / 2);
 
   // LOG(INFO) << "MAIN GOAL";
   // LOG(INFO) << "x: " << goal_state.location.x << " y: " <<
@@ -109,8 +109,8 @@ std::vector<State> MotionPlanner::generate_offset_goals(
     // lie on a perpendicular line to the direction (yaw) of the main goal. You
     // calculated this direction above (yaw_plus_90). HINT: use
     // std::cos(yaw_plus_90) and std::sin(yaw_plus_90)
-    // goal_offset.location.x += ;  // <- Fix This
-    // goal_offset.location.y += ;  // <- Fix This
+    goal_offset.location.x += std::cos(yaw) * offset;
+    goal_offset.location.y += std::sin(yaw) * offset;
     // LOG(INFO) << "x: " << goal_offset.location.x
     //          << " y: " << goal_offset.location.y
     //          << " z: " << goal_offset.location.z
